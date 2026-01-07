dbce0ab4f3924f4c038835c863bad1bd58c6a053
AK github <aalkhareji27@gmail.com>
2026-01-07 23:31:20 +0300

    Revert "Add ASP.NET Core project with JWT auth and SQLite"
    This reverts commit 5628c3a4c221d81aba06ddcaa07dddf7ba8506e7.

--- BRM-back-office-server/Dtos/RegisterRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BRM_back_office_server.Dtos
-{
-    public class RegisterRequest
-    {
-        public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
-    }
-
-}
--- BRM-back-office-server/Dtos/RegisterResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BRM_back_office_server.Dtos
-{
-    public class RegisterResponse
-    {
-        public Guid Id { get; set; }
-        public string Email { get; set; } = null!;
-    }
-
-}
